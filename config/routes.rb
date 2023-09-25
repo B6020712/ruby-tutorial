de9f715b4f3bcb48a0b84bdd63fe081293be5b20
B6020712 <boss123440@hotmail.com>
2023-09-25 14:48:51 +0700

    Hello, Rails!

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # Define your application routes per the DSL(Domain-Specific Language https://en.wikipedia.org/wiki/Domain-specific_language) in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get "/articles", to: "articles#index"
 end
