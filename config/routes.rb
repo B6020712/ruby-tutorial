7146c2d18326cc11e928671ae037931a91745e15
B6020712 <boss123440@hotmail.com>
2023-09-27 10:54:51 +0700

    closed #2, Finish 7.1

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL(Domain-Specific Language https://en.wikipedia.org/wiki/Domain-specific_language) in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
   root "articles#index"
 
   get "/articles", to: "articles#index"
+  get "/articles/:id", to: "articles#show"
 end
